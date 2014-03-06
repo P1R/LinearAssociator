f4df73e8bb8570badd5962f80705d410736507e8
p1r0 <p1r0@insomnia247.nl>
2014-03-06 12:28:53 -0600

    se agregan lineas por error de conversion en numeros y espacio

--- Str2Bin.py
+++ Str2Bin.py
@@ -4,7 +4,10 @@ def Str2Bin(s):
     A=[bin(ord(c))[2:] for c in s]
     B=[]
     for i in range(len(A)):
-        B.append([int(d) for d in str(A[i])])	
+        B.append([int(d) for d in str(A[i])])
+        #condicion para verificar espacios ya que se acotan a longitud de 6
+        if len(B[i])<7:
+            B[i].insert(0, 0)
     return B
 
 def Bin2Str(array):
